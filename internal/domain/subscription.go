025e9d5fcdec011a2c96d34c43e272b02d3f877f
hryak228pizza <I.Pavel-2004@mail.ru>
2026-07-18 23:42:23 +0300

    feat (subscription.go): create data model

---
+++ internal/domain/subscription.go
@@ -0,0 +1,15 @@
+package domain
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type Subscription struct {
+	ID          uuid.UUID
+	ServiceName string
+	Price       int
+	UserID      uuid.UUID
+	StartDate   time.Time
+	EndDate     *time.Time
+}
